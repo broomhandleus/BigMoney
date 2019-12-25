8e889c385803cfae53739f3305737f4c406b95c5
Maxwell broom <maxwellbroom@pop-os.localdomain>
2019-12-25 11:33:20 -0500

    added in fragments. need to figure out the onfragmentinteractionlistener to show the layouts. I think

--- app/src/main/java/com/example/bigmoney/MainActivity.kt
+++ app/src/main/java/com/example/bigmoney/MainActivity.kt
@@ -9,21 +9,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val bottomBarListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.charts -> {
-                println("charts pressed")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.add_money -> {
-                println("add money pressed")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.settings -> {
-                println("settings pressed")
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
+        loadFragment(item.itemId)
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,4 +19,35 @@ class MainActivity : AppCompatActivity() {
         bottom_bar.setOnNavigationItemSelectedListener(bottomBarListener)
     }
 
+    // TODO: Implement the OnFragmentInteractinListener
+
+    private fun loadFragment(itemId: Int) {
+        val tag = itemId.toString()
+        var fragment = supportFragmentManager.findFragmentByTag(tag) ?: when (itemId) {
+            R.id.charts -> {
+                println("Starting up a Chart Fragment")
+                ChartFragment.newInstance()
+            }
+            R.id.add_money -> {
+                println("Starting up an Add Money Fragment")
+                AddFragment.newInstance()
+            }
+            R.id.settings -> {
+                println("Starting up a Settings Fragment")
+                SettingsFragment.newInstance()
+            }
+            else -> {
+                null
+            }
+        }
+
+        // replace fragment
+        if (fragment != null) {
+            println("fragment is not null. Check new instance method")
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment, tag)
+                .commit()
+        }
+    }
 }
