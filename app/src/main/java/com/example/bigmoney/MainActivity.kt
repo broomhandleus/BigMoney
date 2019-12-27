0b4e8ba7acdb50b856b9ec86488aa02a8d7880d2
Maxwell <maximusbroomhandle@gmail.com>
2019-12-27 11:59:37 -0500

    got fragments to show their layouts by converting main activity to constraint layout

--- app/src/main/java/com/example/bigmoney/MainActivity.kt
+++ app/src/main/java/com/example/bigmoney/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.bigmoney
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    AddFragment.OnFragmentInteractionListener,
+    ChartFragment.OnFragmentInteractionListener,
+    SettingsFragment.OnFragmentInteractionListener {
 
     private val bottomBarListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         loadFragment(item.itemId)
@@ -17,9 +21,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_bar.setOnNavigationItemSelectedListener(bottomBarListener)
-    }
 
-    // TODO: Implement the OnFragmentInteractinListener
+        // Start the app in the Add Purchase Fragment
+        if (savedInstanceState == null) {
+            val fragment = AddFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment, fragment.tag)
+                .commit()
+        }
+    }
 
     private fun loadFragment(itemId: Int) {
         val tag = itemId.toString()
@@ -49,5 +58,9 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fragmentContainer, fragment, tag)
                 .commit()
         }
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        // No implementation at this point
     }
 }
